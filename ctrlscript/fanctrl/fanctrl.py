35fb388a3e88f3756eb4648ed1b2469b5a8da9a9
LPGUAIA <2158494709@qq.com>
2022-10-14 05:33:12 +0000

    写了个大bug修了修

--- ctrlscript/fanctrl/fanctrl.py
+++ ctrlscript/fanctrl/fanctrl.py
@@ -66,18 +66,19 @@ class FAN:
             set_value2path(userset_expecttemp, self.exptemp)
 
         if (self.cpu_temp > self.walltemp):
+            #print(fan.cpu_temp)
             self.set_power_percent(self.maxpower)
 
         elif (self.cpu_temp < self.exptemp):
-            set_value2path(fan_pwm_duty_cycle_set_path,
-                       '0')
+            #print(fan.walltemp)
+            self.set_power_percent(0)
 
 
 
 # check if registered pwm device
 set_value2path(fan_pwm_reg_path, '0')
 set_value2path(fan_pwm_peri_set_path, read_valuefrompath(
-    userset_fan_pwm_cycle))  # set pwm period
+    userset_fan_pwm_period))  # set pwm period
 set_value2path(fan_pwm_duty_cycle_set_path, read_valuefrompath(
     userset_fan_dead_duty_cycle))  # set to initial pwm
 set_value2path(fan_pwm_switch_set_path, '1')  # enable pwm
@@ -89,7 +90,9 @@ fan = FAN()
 while True:
     fan.__init__()
     if (fan.mod == 0):
+        print(fan.cpu_temp)
         set_value2path(read_valuefrompath(userset_fan_pwm_cycle))
     else:
         fan.fan_switch_ctrl()
+        
     sleep(2)
