839184d8647f5048a894226130ddd2bdafa561ad
LPGUAIA <2158494709@qq.com>
2022-12-06 19:28:02 +0000

    更正版本

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rock/kekeandzuozuo4rockpi5b/ctrlscript/fanctrl
-# Build directory: /home/rock/kekeandzuozuo4rockpi5b/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
